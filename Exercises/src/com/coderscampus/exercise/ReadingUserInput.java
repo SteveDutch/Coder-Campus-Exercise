3914ebd639698f554a9483c3b0d7a233df5612a1
SteveDutch <steholl@posteo.org>
2022-02-13 22:23:41 +0100

    Initialize

---
+++ Exercises/src/com/coderscampus/exercise/ReadingUserInput.java
@@ -0,0 +1,27 @@
+package com.coderscampus.exercise;
+
+import java.util.Scanner;
+
+public class ReadingUserInput {
+//vermutlich nicht schön, but it works
+	public static void main (String[] args) {
+		//wow! thanks to an IDE, daher der Vorschlag des TRY (und mehr verstanden habe ich es dadurch auch)
+		try (Scanner readInput = new Scanner (System.in)) {		
+			System.out.println("Type a number between 50 and 300");
+			String userInput = readInput.nextLine();
+			Integer convertedUserInput = Integer.parseInt(userInput);
+			readInput.close();
+			
+			if (convertedUserInput >= 50 && convertedUserInput <= 300) {
+				System.out.println("Yes!");
+			} else {System.out.println("No!");
+
+}
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			System.out.println("that wasn't a number! programm terminates with showing the Stacktrace now!!");
+			e.printStackTrace();
+		}
+	
+}
+}
