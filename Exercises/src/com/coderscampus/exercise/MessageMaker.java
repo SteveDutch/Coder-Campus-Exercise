331f1c6d1e3be693b0a81bb6a4a913f951e5b681
SteveDutch <steholl@posteo.org>
2022-02-14 15:30:21 +0100

    work in progress

---
+++ Exercises/src/com/coderscampus/exercise/MessageMaker.java
@@ -0,0 +1,13 @@
+package com.coderscampus.exercise;
+
+public class MessageMaker {
+	public Integer getMessage(Integer convertedUserInput) {
+		if (convertedUserInput >= 50 && convertedUserInput <= 300) {
+			return convertedUserInput;
+		} else {return null;
+			
+		}
+
+	}
+
+}
--- Exercises/src/com/coderscampus/exercise/ReadingUserInput.java
+++ Exercises/src/com/coderscampus/exercise/ReadingUserInput.java
@@ -6,22 +6,19 @@ public class ReadingUserInput {
 //vermutlich nicht schön, but it works
 	public static void main (String[] args) {
 		//wow! thanks to an IDE, daher der Vorschlag des TRY (und mehr verstanden habe ich es dadurch auch)
-		try (Scanner readInput = new Scanner (System.in)) {		
+		Scanner readInput = new Scanner (System.in);		
 			System.out.println("Type a number between 50 and 300");
 			String userInput = readInput.nextLine();
 			Integer convertedUserInput = Integer.parseInt(userInput);
 			readInput.close();
+			System.out.println(convertedUserInput.getMessage());			
 			
-			if (convertedUserInput >= 50 && convertedUserInput <= 300) {
-				System.out.println("Yes!");
-			} else {System.out.println("No!");
-
-}
-		} catch (NumberFormatException e) {
+			 /*catch (NumberFormatException e) {
 			// TODO Auto-generated catch block
 			System.out.println("that wasn't a number! programm terminates with showing the Stacktrace now!!");
 			e.printStackTrace();
-		}
+		}*/
+	}
+
+}
 	
-}
-}
