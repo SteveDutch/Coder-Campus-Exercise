584c3f731da69d259fa68298c701d034f51339ab
SteveDutch <steholl@posteo.org>
2022-02-14 19:26:15 +0100

    finished and working

--- Exercises/src/com/coderscampus/exercise/ReadingUserInput.java
+++ Exercises/src/com/coderscampus/exercise/ReadingUserInput.java
@@ -5,19 +5,22 @@ import java.util.Scanner;
 public class ReadingUserInput {
 //vermutlich nicht schön, but it works
 	public static void main (String[] args) {
-		//wow! thanks to an IDE, daher der Vorschlag des TRY (und mehr verstanden habe ich es dadurch auch)
 		Scanner readInput = new Scanner (System.in);		
 			System.out.println("Type a number between 50 and 300");
 			String userInput = readInput.nextLine();
 			Integer convertedUserInput = Integer.parseInt(userInput);
+			Object output = getMessage(convertedUserInput);
+			System.out.println(output);			
 			readInput.close();
-			System.out.println(convertedUserInput.getMessage());			
 			
-			 /*catch (NumberFormatException e) {
-			// TODO Auto-generated catch block
-			System.out.println("that wasn't a number! programm terminates with showing the Stacktrace now!!");
-			e.printStackTrace();
-		}*/
+	}
+
+	private static Object getMessage(Integer param) { 
+		if (param >= 50 && param <= 300) {
+		  return param; 
+		  } else {param = null;} 
+		return param;
+  
 	}
 
 }
