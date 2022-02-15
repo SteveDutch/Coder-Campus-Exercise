89fc06d67c8c531f1bc390a97d91b46ad65a57d9
SteveDutch <steholl@posteo.org>
2022-02-15 19:05:22 +0100

    polish

--- Exercises/src/com/coderscampus/exercise/ReadingUserInput.java
+++ Exercises/src/com/coderscampus/exercise/ReadingUserInput.java
@@ -6,30 +6,22 @@ public class ReadingUserInput {
 
 	public static void main (String[] args) {
 		System.out.println("Type a number between 50 and 300");
-		//int output = 1;
-		//whileLoop();
-		// Scanner readInput = new Scanner (System.in);
-		
-//			String userInput = readInput.nextLine();
-//			Integer convertedUserInput = Integer.parseInt(userInput);
-			// Object output = whileLoop(convertedUserInput);
 			
 			System.out.println("The number you typed in was: " + whileLoop());
-//			readInput.close();
 
 	}
 
 	private static int whileLoop() {
-		//int convertedUserInput = 1;
-		
+				
 		Scanner readInput = new Scanner (System.in);
 		String userInput = readInput.nextLine();
 		Integer convertedUserInput = Integer.parseInt(userInput);
-		System.out.println(convertedUserInput + "vor");
+		System.out.println(convertedUserInput + "before loop");
+		
 		while (convertedUserInput > 300 || convertedUserInput < 50 ) {
 			System.out.println("Oops, that number wasn't between 50 and 300, try again:");
-			System.out.println(convertedUserInput + "hinter");
-			convertedUserInput = 1;
+			System.out.println(convertedUserInput + "inside loop");
+			
 			whileLoop();
 		  } 
 		int output = convertedUserInput;
