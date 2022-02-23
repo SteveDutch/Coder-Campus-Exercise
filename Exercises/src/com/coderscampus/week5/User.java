9ad8f9b46b5327c6ca470d9cbec78cbcc883d6de
SteveDutch <steholl@posteo.org>
2022-02-23 23:22:35 +0100

    start

---
+++ Exercises/src/com/coderscampus/week5/User.java
@@ -0,0 +1,15 @@
+package com.coderscampus.week5;
+
+public class User {
+	
+	private String username;
+	private String password;
+	private String name;
+
+	public User(String username, String password, String name) {
+		this.username = username;
+		this.password = password;
+		this.name = name;
+	}
+
+}
---
+++ Exercises/src/com/coderscampus/week5/UserService.java
@@ -0,0 +1,13 @@
+package com.coderscampus.week5;
+
+public class UserService {
+	
+	public User createUser (stringInput[0] = "aUsernameHere",
+	stringInput[1] = "aPasswordHere",
+	stringInput[2] = "Somebodies Name") {
+		User aUser = new User;
+		
+		return aUser
+	}
+
+}
